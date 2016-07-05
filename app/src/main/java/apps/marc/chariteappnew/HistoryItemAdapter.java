dd0b12edf613baa46328448e7a1949028789086f
mhorl <marc.horlacher@gmx.de>
2016-07-05 23:08:44 +0200

    CheckBox Problems

--- app/src/main/java/apps/marc/chariteappnew/HistoryItemAdapter.java
+++ app/src/main/java/apps/marc/chariteappnew/HistoryItemAdapter.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.CheckBox;
 
 import java.util.ArrayList;
 import java.text.DateFormat;
@@ -16,19 +18,50 @@ import java.text.SimpleDateFormat;
 
 public class HistoryItemAdapter extends ArrayAdapter<MealEntry>{
 
+    private final ArrayList<MealEntry> historyItems;
+    private final boolean[] checkboxState;
+    private final Context context;
+
     public HistoryItemAdapter(Context context, ArrayList<MealEntry> historyItems) {
         super(context, R.layout.history_item_template, historyItems);
+
+        this.context = context;
+        this.historyItems = historyItems;
+        this.checkboxState = new boolean[this.historyItems.size()];
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater layoutInflater = LayoutInflater.from(getContext());
-        View itemView = layoutInflater.inflate(R.layout.history_item_template, parent, false);
+        convertView = layoutInflater.inflate(R.layout.history_item_template, parent, false);
+
+        //Checkbox-handling
+
+        CheckBox checkBox;
+
+        if(convertView == null){
+            checkBox = (CheckBox) convertView.findViewById(R.id.historyItem_checkBox);
+
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    int boxPosition = (Integer) buttonView.getTag();
+                    historyItems.get(boxPosition).setChecked(buttonView.isChecked());
+                }
+            });
+        }
+        else{
+            checkBox = (CheckBox) convertView.getTag();
+        }
+
+        checkBox.setChecked(true); //historyItems.get(position).isChecked()
+
+        //*****************
 
         MealEntry historyItem = getItem(position);
-        TextView historyItem_largeText = (TextView) itemView.findViewById(R.id.historyItem_largeText);
-        TextView historyItem_smallText = (TextView) itemView.findViewById(R.id.historyItem_smallText);
-        ImageView historyItem_image = (ImageView) itemView.findViewById(R.id.historyItem_image);
+        TextView historyItem_largeText = (TextView) convertView.findViewById(R.id.historyItem_largeText);
+        TextView historyItem_smallText = (TextView) convertView.findViewById(R.id.historyItem_smallText);
+        ImageView historyItem_image = (ImageView) convertView.findViewById(R.id.historyItem_image);
 
         historyItem_largeText.setText(historyItem.getMealName());
         historyItem_largeText.setTextColor(Color.BLACK);
@@ -39,6 +72,6 @@ public class HistoryItemAdapter extends ArrayAdapter<MealEntry>{
 
         historyItem_image.setImageResource(R.drawable.meal_icon);
 
-        return itemView;
+        return convertView;
     }
 }
